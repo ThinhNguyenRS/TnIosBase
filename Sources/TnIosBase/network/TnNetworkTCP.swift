e9bf0c24540d7a0a29d08079448c95f3a41f5bbe
Thinh Nguyen <thinhnguyen.risinglights@gmail.com>
2024-09-13 17:32:49 +0700

    apply sending queue

--- Sources/TnIosBase/network/TnNetworkTCP.swift
+++ Sources/TnIosBase/network/TnNetworkTCP.swift
@@ -307,10 +307,10 @@ public class TnNetworkConnection: TnNetwork, TnTransportableProtocol {
     private func startSend() {
         if let data = sendingQueue.first {
             sendChunk(data) { [self] in
-//                if !sendingQueue.isEmpty {
-//                    sendingQueue.removeFirst()
-//                }
-                sendingQueue.removeFirst()
+                if !sendingQueue.isEmpty {
+                    sendingQueue.removeFirst()
+                }
+//                sendingQueue.removeFirst()
                 if !sendingQueue.isEmpty {
                     Thread.sleep(forTimeInterval: 0.1)
                     startSend()
