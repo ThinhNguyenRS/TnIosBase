af11ecdf2358f2627d6e8977f05d930ff5f7668f
Thinh Nguyen <thinhnguyen.risinglights@gmail.com>
2024-09-24 06:36:14 +0700

    picker

--- Sources/TnIosBase/views/TnPickerField.swift
+++ Sources/TnIosBase/views/TnPickerField.swift
@@ -21,7 +21,8 @@ public struct TnPickerField<T: Hashable & Comparable, TStyle: PickerStyle>: View
         values: [T],
         labels: [String],
         style: @escaping () -> TStyle,
-        onChanged: ((T) -> Void)? = nil)
+        onChanged: ((T) -> Void)? = nil
+    )
     {
         self.label = label
         self.values = values
@@ -48,170 +49,255 @@ public struct TnPickerField<T: Hashable & Comparable, TStyle: PickerStyle>: View
     }
 }
 
-extension TnPickerField where T: TnEnum, TStyle ==  SegmentedPickerStyle {
-    public static func forEnum(label: String, value: Binding<T>, onChanged: ((T) -> Void)? = nil) -> TnPickerField {
-        TnPickerField(
-            label: label,
-            value: value,
-            values: T.allCases,
-            labels: T.allNames,
-            style: { SegmentedPickerStyle() },
-            onChanged: onChanged
-        )
-    }
-    
-    public static func forEnum(label: String, value: Binding<T>, values: [T], onChanged: ((T) -> Void)? = nil) -> TnPickerField {
-        TnPickerField(
-            label: label,
-            value: value,
-            values: values,
-            labels: values.descriptions,
-            style: { SegmentedPickerStyle() },
-            onChanged: onChanged
-        )
-    }
-}
-
-extension TnPickerField where T == String {
-    var pickerLabel: String {
-        value.wrappedValue
-    }
-}
-
-
-public func tnPickerFieldEnum<T: TnEnum, TStyle: PickerStyle>(label: String, value: Binding<T>, style: @escaping () -> TStyle, onChanged: ((T) -> Void)? = nil) -> TnPickerField<T, TStyle> {
-    TnPickerField(
-        label: label,
-        value: value,
-        values: T.allCases,
-        labels: T.allNames,
-        style: style,
-        onChanged: onChanged)
-}
-
-public func tnPickerFieldEnum<T: TnEnum>(label: String, value: Binding<T>, onChanged: ((T) -> Void)? = nil) -> TnPickerField<T, SegmentedPickerStyle> {
-    TnPickerField(
-        label: label,
-        value: value,
-        values: T.allCases,
-        labels: T.allNames,
-        style: {SegmentedPickerStyle()},
-        onChanged: onChanged
-    )
-}
-
-public func tnPickerFieldString<TStyle: PickerStyle>(label: String, value: Binding<String>, labels: [String], style: @escaping () -> TStyle, onChanged: ((String) -> Void)? = nil) -> TnPickerField<String, TStyle> {
-    TnPickerField(
-        label: label,
-        value: value,
-        values: labels,
-        labels: labels,
-        style: style,
-        onChanged: onChanged
-    )
-}
-
-public func tnPickerFieldString(label: String, value: Binding<String>, labels: [String], onChanged: ((String) -> Void)? = nil) -> TnPickerField<String, SegmentedPickerStyle> {
-    TnPickerField(
-        label: label,
-        value: value,
-        values: labels,
-        labels: labels,
-        style: {SegmentedPickerStyle()}
-    )
-}
-
-public func tnPickerFieldStringMenu(label: String, value: Binding<String>, labels: [String], onChanged: ((String) -> Void)? = nil) -> TnPickerField<String, MenuPickerStyle> {
-    TnPickerField(
-        label: label,
-        value: value,
-        values: labels,
-        labels: labels,
-        style: {MenuPickerStyle()},
-        onChanged: onChanged
-    )
-}
-
-public struct TnPickerFieldPopup<TValue: Hashable & Comparable>: View {
-    let label: String
-    let value: Binding<TValue>
-    let values: [TValue]
-    let labels: [String]
-    var onChanged: ((TValue) -> Void)? = nil
-    
-    @State private var valueIndex = 0
-    @State private var showPopup = false
-
-    public init(label: String, value: Binding<TValue>, values: [TValue], labels: [String], onChanged: ((TValue) -> Void)? = nil) {
-        self.label = label
-        self.value = value
-        self.values = values
-        self.labels = labels
-        self.onChanged = onChanged
-    }
-    
-    public var body: some View {
-        HStack {
-            tnText(label)
-            Spacer()
-            if labels.count > valueIndex {
-                Text(labels[valueIndex])
-                    .foregroundColor(.blue)
+extension View {
+    public func tnPickerView<T: Hashable & Comparable>(
+        value: Binding<T>,
+        values: [T],
+        labels: [String],
+        onChanged: ((T) -> Void)? = nil
+    ) -> some View {
+        Picker("", selection: value.projectedValue) {
+            tnForEach(values) { idx, value in
+                tnText(labels[idx])
+                    .tag(value)
             }
         }
-        .onTapGesture {
-            showPopup = true
-        }
-        .sheet(isPresented: $showPopup, content: {
-            VStack {
-                tnText(label)
-                    .bold()
-                    .foregroundColor(.TKG)
-                    .padding()
-
-                Spacer()
-
-                if #available(iOS 17.0, *) {
-                    Picker(label, selection: $valueIndex) {
-                        tnForEach(values) { idx, value in
-                            tnText(labels[idx])
-                                .tag(idx)
-                        }
-                    }
-                    .pickerStyle(WheelPickerStyle())
-                    .onChange(of: valueIndex) {
-                        value.wrappedValue = values[valueIndex]
-                        onChanged?(value.wrappedValue)
-                    }
-                } else {
-                    // Fallback on earlier versions
-                    Picker(label, selection: $valueIndex) {
-                        tnForEach(values) { idx, value in
-                            tnText(labels[idx])
-                                .tag(idx)
-                        }
-                    }
-                    .pickerStyle(WheelPickerStyle())
-                    .onChange(of: valueIndex, perform: { newIndex in
-                        value.wrappedValue = values[newIndex]
-                        onChanged?(value.wrappedValue)
-                    })
-                }
-                
-                Spacer()
-                tnTextButton("Close", action: {
-                    showPopup = false
-                })
-                .padding()
-            }
+        .onChange(of: value.wrappedValue, perform: { _ in
+            onChanged?(value.wrappedValue)
         })
-        .onAppear {
-            value.wrappedValue = value.wrappedValue.valueInRange(values)
-            if !values.isEmpty {
-                valueIndex = values.firstIndex(of: value.wrappedValue)!
-            }
+    }
+    
+    public func tnPickerView<T: TnEnum>(
+        value: Binding<T>,
+        values: [T]? = nil,
+        onChanged: ((T) -> Void)? = nil
+    ) -> some View {
+        let valuesToUse = values ?? T.allCases
+        let labels = valuesToUse.descriptions
+        
+        return tnPickerView(
+            value: value,
+            values: valuesToUse,
+            labels: labels,
+            onChanged: onChanged
+        )
+    }
+    
+    public func tnPickerViewHorz<T: Hashable & Comparable>(
+        label: String,
+        value: Binding<T>,
+        values: [T],
+        labels: [String],
+        onChanged: ((T) -> Void)? = nil
+    ) -> some View {
+        HStack {
+            Text(label)
+            tnPickerView(value: value, values: values, labels: labels)
+        }
+    }
+    
+    public func tnPickerViewHorz<T: TnEnum>(
+        label: String,
+        value: Binding<T>,
+        values: [T]? = nil,
+        onChanged: ((T) -> Void)? = nil
+    ) -> some View {
+        HStack {
+            Text(label)
+            tnPickerView(value: value, values: values)
+        }
+    }
+    
+    public func tnPickerViewVert<T: Hashable & Comparable>(
+        label: String,
+        value: Binding<T>,
+        values: [T],
+        labels: [String],
+        onChanged: ((T) -> Void)? = nil
+    ) -> some View {
+        VStack(alignment: .leading) {
+            Text(label)
+            tnPickerView(value: value, values: values, labels: labels)
+        }
+    }
+    
+    public func tnPickerViewVert<T: TnEnum>(
+        label: String,
+        value: Binding<T>,
+        values: [T]? = nil,
+        onChanged: ((T) -> Void)? = nil
+    ) -> some View {
+        VStack(alignment: .leading) {
+            Text(label)
+            tnPickerView(value: value, values: values)
         }
     }
 }
 
-
+//extension TnPickerField where T: TnEnum, TStyle ==  SegmentedPickerStyle {
+//    public static func forEnum(label: String, value: Binding<T>, onChanged: ((T) -> Void)? = nil) -> TnPickerField {
+//        TnPickerField(
+//            label: label,
+//            value: value,
+//            values: T.allCases,
+//            labels: T.allNames,
+//            style: { SegmentedPickerStyle() },
+//            onChanged: onChanged
+//        )
+//    }
+//    
+//    public static func forEnum(label: String, value: Binding<T>, values: [T], onChanged: ((T) -> Void)? = nil) -> TnPickerField {
+//        TnPickerField(
+//            label: label,
+//            value: value,
+//            values: values,
+//            labels: values.descriptions,
+//            style: { SegmentedPickerStyle() },
+//            onChanged: onChanged
+//        )
+//    }
+//}
+//
+//extension TnPickerField where T == String {
+//    var pickerLabel: String {
+//        value.wrappedValue
+//    }
+//}
+//
+//
+//public func tnPickerFieldEnum<T: TnEnum, TStyle: PickerStyle>(label: String, value: Binding<T>, style: @escaping () -> TStyle, onChanged: ((T) -> Void)? = nil) -> TnPickerField<T, TStyle> {
+//    TnPickerField(
+//        label: label,
+//        value: value,
+//        values: T.allCases,
+//        labels: T.allNames,
+//        style: style,
+//        onChanged: onChanged)
+//}
+//
+//public func tnPickerFieldEnum<T: TnEnum>(label: String, value: Binding<T>, onChanged: ((T) -> Void)? = nil) -> TnPickerField<T, SegmentedPickerStyle> {
+//    TnPickerField(
+//        label: label,
+//        value: value,
+//        values: T.allCases,
+//        labels: T.allNames,
+//        style: {SegmentedPickerStyle()},
+//        onChanged: onChanged
+//    )
+//}
+//
+//public func tnPickerFieldString<TStyle: PickerStyle>(label: String, value: Binding<String>, labels: [String], style: @escaping () -> TStyle, onChanged: ((String) -> Void)? = nil) -> TnPickerField<String, TStyle> {
+//    TnPickerField(
+//        label: label,
+//        value: value,
+//        values: labels,
+//        labels: labels,
+//        style: style,
+//        onChanged: onChanged
+//    )
+//}
+//
+//public func tnPickerFieldString(label: String, value: Binding<String>, labels: [String], onChanged: ((String) -> Void)? = nil) -> TnPickerField<String, SegmentedPickerStyle> {
+//    TnPickerField(
+//        label: label,
+//        value: value,
+//        values: labels,
+//        labels: labels,
+//        style: {SegmentedPickerStyle()}
+//    )
+//}
+//
+//public func tnPickerFieldStringMenu(label: String, value: Binding<String>, labels: [String], onChanged: ((String) -> Void)? = nil) -> TnPickerField<String, MenuPickerStyle> {
+//    TnPickerField(
+//        label: label,
+//        value: value,
+//        values: labels,
+//        labels: labels,
+//        style: {MenuPickerStyle()},
+//        onChanged: onChanged
+//    )
+//}
+//
+//public struct TnPickerFieldPopup<TValue: Hashable & Comparable>: View {
+//    let label: String
+//    let value: Binding<TValue>
+//    let values: [TValue]
+//    let labels: [String]
+//    var onChanged: ((TValue) -> Void)? = nil
+//    
+//    @State private var valueIndex = 0
+//    @State private var showPopup = false
+//
+//    public init(label: String, value: Binding<TValue>, values: [TValue], labels: [String], onChanged: ((TValue) -> Void)? = nil) {
+//        self.label = label
+//        self.value = value
+//        self.values = values
+//        self.labels = labels
+//        self.onChanged = onChanged
+//    }
+//    
+//    public var body: some View {
+//        HStack {
+//            tnText(label)
+//            Spacer()
+//            if labels.count > valueIndex {
+//                Text(labels[valueIndex])
+//                    .foregroundColor(.blue)
+//            }
+//        }
+//        .onTapGesture {
+//            showPopup = true
+//        }
+//        .sheet(isPresented: $showPopup, content: {
+//            VStack {
+//                tnText(label)
+//                    .bold()
+//                    .foregroundColor(.TKG)
+//                    .padding()
+//
+//                Spacer()
+//
+//                if #available(iOS 17.0, *) {
+//                    Picker(label, selection: $valueIndex) {
+//                        tnForEach(values) { idx, value in
+//                            tnText(labels[idx])
+//                                .tag(idx)
+//                        }
+//                    }
+//                    .pickerStyle(WheelPickerStyle())
+//                    .onChange(of: valueIndex) {
+//                        value.wrappedValue = values[valueIndex]
+//                        onChanged?(value.wrappedValue)
+//                    }
+//                } else {
+//                    // Fallback on earlier versions
+//                    Picker(label, selection: $valueIndex) {
+//                        tnForEach(values) { idx, value in
+//                            tnText(labels[idx])
+//                                .tag(idx)
+//                        }
+//                    }
+//                    .pickerStyle(WheelPickerStyle())
+//                    .onChange(of: valueIndex, perform: { newIndex in
+//                        value.wrappedValue = values[newIndex]
+//                        onChanged?(value.wrappedValue)
+//                    })
+//                }
+//                
+//                Spacer()
+//                tnTextButton("Close", action: {
+//                    showPopup = false
+//                })
+//                .padding()
+//            }
+//        })
+//        .onAppear {
+//            value.wrappedValue = value.wrappedValue.valueInRange(values)
+//            if !values.isEmpty {
+//                valueIndex = values.firstIndex(of: value.wrappedValue)!
+//            }
+//        }
+//    }
+//}
+//
+//
