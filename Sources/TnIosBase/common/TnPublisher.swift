e1e0bd4c82bbaae974994956ce5e7126e7504690
Thinh Nguyen <thinhnguyen.risinglights@gmail.com>
2024-09-23 17:32:32 +0700

    publisher

--- Sources/TnIosBase/common/TnPublisher.swift
+++ Sources/TnIosBase/common/TnPublisher.swift
@@ -11,7 +11,7 @@ import Combine
 extension Published.Publisher {
     public func onReceive(queue: DispatchQueue, debounceMs: Double, cancelables: inout Set<AnyCancellable>, handler: @escaping (Self.Output) -> Void) {
         self
-//            .debounce(for: .seconds(debounceMs/1000), scheduler: queue)
+            .debounce(for: .seconds(debounceMs/1000), scheduler: queue)
             .receive(on: queue)
             .sink(receiveValue: handler)
             .store(in: &cancelables)
@@ -19,7 +19,7 @@ extension Published.Publisher {
     
     public func onReceive(debounceMs: Double, cancelables: inout Set<AnyCancellable>, handler: @escaping (Self.Output) -> Void) {
         self
-//            .debounce(for: .seconds(debounceMs/1000), scheduler: DispatchQueue.main)
+            .debounce(for: .seconds(debounceMs/1000), scheduler: DispatchQueue.main)
             .receive(on: DispatchQueue.main)
             .sink(receiveValue: handler)
             .store(in: &cancelables)
