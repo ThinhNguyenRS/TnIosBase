5af771cf6bbfd5c3411c951bb04965c718f55ca9
Thinh Nguyen <thinhnguyen.risinglights@gmail.com>
2024-09-24 16:30:13 +0700

    cancellables

--- Sources/TnIosBase/common/TnPublisher.swift
+++ Sources/TnIosBase/common/TnPublisher.swift
@@ -9,34 +9,34 @@ import Foundation
 import Combine
 
 extension Published.Publisher {
-    public func onReceive(queue: DispatchQueue, debounceMs: Double, cancelables: inout Set<AnyCancellable>, handler: @escaping (Self.Output) -> Void) {
+    public func onReceive(queue: DispatchQueue, debounceMs: Double, cancellables: inout Set<AnyCancellable>, handler: @escaping (Self.Output) -> Void) {
         self
             .debounce(for: .seconds(debounceMs/1000), scheduler: queue)
             .receive(on: queue)
             .sink(receiveValue: handler)
-            .store(in: &cancelables)
+            .store(in: &cancellables)
     }
     
-    public func onReceive(debounceMs: Double, cancelables: inout Set<AnyCancellable>, handler: @escaping (Self.Output) -> Void) {
+    public func onReceive(debounceMs: Double, cancellables: inout Set<AnyCancellable>, handler: @escaping (Self.Output) -> Void) {
         self
             .debounce(for: .seconds(debounceMs/1000), scheduler: DispatchQueue.main)
             .receive(on: DispatchQueue.main)
             .sink(receiveValue: handler)
-            .store(in: &cancelables)
+            .store(in: &cancellables)
     }
     
-    public func onReceive(queue: DispatchQueue, cancelables: inout Set<AnyCancellable>, handler: @escaping (Self.Output) -> Void) {
+    public func onReceive(queue: DispatchQueue, cancellables: inout Set<AnyCancellable>, handler: @escaping (Self.Output) -> Void) {
         self
             .receive(on: queue)
             .sink(receiveValue: handler)
-            .store(in: &cancelables)
+            .store(in: &cancellables)
     }
 
-    public func onReceive(cancelables: inout Set<AnyCancellable>, handler: @escaping (Self.Output) -> Void) {
+    public func onReceive(cancellables: inout Set<AnyCancellable>, handler: @escaping (Self.Output) -> Void) {
         self
             .receive(on: DispatchQueue.main)
             .sink(receiveValue: handler)
-            .store(in: &cancelables)
+            .store(in: &cancellables)
     }
 }
 
