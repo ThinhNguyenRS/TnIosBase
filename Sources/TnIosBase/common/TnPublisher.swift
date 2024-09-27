8a949b705fbdb855a513d24ffd0ff40da19bbdca
Thinh Nguyen <thinhnguyen.risinglights@gmail.com>
2024-09-27 09:20:22 +0700

    cancellables

--- Sources/TnIosBase/common/TnPublisher.swift
+++ Sources/TnIosBase/common/TnPublisher.swift
@@ -8,6 +8,8 @@
 import Foundation
 import Combine
 
+public var tnGlobalCancellables: Set<AnyCancellable> = .init()
+
 extension Published.Publisher {
     public func onReceive(queue: DispatchQueue, debounceMs: Double, cancellables: inout Set<AnyCancellable>, handler: @escaping (Self.Output) -> Void) {
         self
@@ -15,6 +17,14 @@ extension Published.Publisher {
             .receive(on: queue)
             .sink(receiveValue: handler)
             .store(in: &cancellables)
+    }
+    
+    public func onReceive(queue: DispatchQueue, debounceMs: Double, handler: @escaping (Self.Output) -> Void) {
+        self
+            .debounce(for: .seconds(debounceMs/1000), scheduler: queue)
+            .receive(on: queue)
+            .sink(receiveValue: handler)
+            .store(in: &tnGlobalCancellables)
     }
     
     public func onReceive(debounceMs: Double, cancellables: inout Set<AnyCancellable>, handler: @escaping (Self.Output) -> Void) {
@@ -25,18 +35,39 @@ extension Published.Publisher {
             .store(in: &cancellables)
     }
     
+    public func onReceive(debounceMs: Double, handler: @escaping (Self.Output) -> Void) {
+        self
+            .debounce(for: .seconds(debounceMs/1000), scheduler: DispatchQueue.main)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: handler)
+            .store(in: &tnGlobalCancellables)
+    }
+    
     public func onReceive(queue: DispatchQueue, cancellables: inout Set<AnyCancellable>, handler: @escaping (Self.Output) -> Void) {
         self
             .receive(on: queue)
             .sink(receiveValue: handler)
             .store(in: &cancellables)
     }
-
+    
+    public func onReceive(queue: DispatchQueue, handler: @escaping (Self.Output) -> Void) {
+        self
+            .receive(on: queue)
+            .sink(receiveValue: handler)
+            .store(in: &tnGlobalCancellables)
+    }
+    
     public func onReceive(cancellables: inout Set<AnyCancellable>, handler: @escaping (Self.Output) -> Void) {
         self
             .receive(on: DispatchQueue.main)
             .sink(receiveValue: handler)
             .store(in: &cancellables)
     }
+    
+    public func onReceive(handler: @escaping (Self.Output) -> Void) {
+        self
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: handler)
+            .store(in: &tnGlobalCancellables)
+    }
 }
-
