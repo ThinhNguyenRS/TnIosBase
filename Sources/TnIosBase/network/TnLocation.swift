2aa2255a422ba2fee2727cf64f626169bbe27b8e
Thinh Nguyen <thinhnguyen.risinglights@gmail.com>
2024-10-04 23:45:19 +0700

    location

---
+++ Sources/TnIosBase/network/TnLocation.swift
@@ -0,0 +1,53 @@
+//
+//  TnLocation.swift
+//  TnIosBase
+//
+//  Created by Thinh Nguyen on 10/4/24.
+//
+
+import Foundation
+import CoreLocation
+
+public class TnLocation: NSObject, ObservableObject {
+    private let locationManager: CLLocationManager
+    private var completion: ((CLLocation?) -> Void)? = nil
+    
+    @Published public private(set) var location: CLLocation? = nil
+
+    public override init() {
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        if locationManager.authorizationStatus == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    public func start(completion: @escaping (CLLocation?) -> Void) {
+        self.completion = completion
+        locationManager.startUpdatingLocation()
+    }
+    
+    public func stop() {
+        locationManager.stopUpdatingLocation()
+    }
+    
+    public func request(completion: @escaping (CLLocation?) -> Void) {
+        self.completion = completion
+        locationManager.requestLocation()
+    }
+    
+    public func request() async -> CLLocation? {
+        await withCheckedContinuation { continuation in
+            self.request { location in
+                continuation.resume(returning: location)
+            }
+        }
+    }
+}
+
+extension TnLocation: CLLocationManagerDelegate {
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.first
+        completion?(locations.first)
+    }
+}
