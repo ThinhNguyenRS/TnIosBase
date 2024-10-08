32bb97207f9bbaa755916ab6d1b34976a4249965
Thinh Nguyen <thinhnguyen.risinglights@gmail.com>
2024-10-08 19:56:11 +0700

    host info

--- Sources/TnIosBase/network/TnNetworkTCP.swift
+++ Sources/TnIosBase/network/TnNetworkTCP.swift
@@ -30,7 +30,7 @@ public struct TnNetworkHostInfo: Codable {
     public let host: String
     public let port: UInt16
     
-    init(host: String, port: UInt16) {
+    public init(host: String, port: UInt16) {
         self.host = host
         self.port = port
     }
